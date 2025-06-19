d709e104102657f7154e7e6d23fcfdff55075f96
pacoquiroga <pacoquiroga33@gmail.com>
2025-06-19 10:50:12 -0500

    epa

---
+++ Menu/menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://niveles/nivel_1.tscn")
+	
+
+func _on_configuraciones_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()# Replace with function body.
