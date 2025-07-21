3a5fb86b74b0b1bf3a9c8dc08ee84458277c1f7e
pacoquiroga <pacoquiroga33@gmail.com>
2025-07-21 18:30:19 -0500

    Menus

--- Menu/menu.gd
+++ Menu/menu.gd
@@ -4,10 +4,13 @@ extends Control
 
 
 func _on_jugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://niveles/nivel_1.tscn")
+	get_tree().change_scene_to_file("res://Menu/SeleccionarNivel.tscn")
 	
-
 
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_configuraciones_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menu/settings.tscn")
